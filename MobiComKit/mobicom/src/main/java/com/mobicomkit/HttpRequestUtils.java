f90c23718ec2fbcf7a748f1645e89a21dec3fad8
devashishmamgain <devashish.mamgain@gmail.com>
2015-03-23 21:12:04 +0530

    #522 MobiComKitUI code for ui

--- MobiComKit/mobicom/src/main/java/com/mobicomkit/HttpRequestUtils.java
+++ MobiComKit/mobicom/src/main/java/com/mobicomkit/HttpRequestUtils.java
@@ -40,7 +40,6 @@ public class HttpRequestUtils {
     //public static String APPLICATION_KEY_HEADER_VALUE = "mobitexter-texting-from-pc";
     public static String APPLICATION_KEY_HEADER_VALUE = "992a65190e8da391740e47537";
 
-
     private static String SOURCE_HEADER = "Source";
 
     private static String SOURCE_HEADER_VALUE = "1";
